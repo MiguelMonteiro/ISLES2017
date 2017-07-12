added quick fix for exploding loss (gradients were not exploding maybe) need better solution

trainer/model.py
@@ -63,8 +63,8 @@ def model_fn(tf_input_data, tf_ground_truth, n_channels):
         learning_rate = tf.train.exponential_decay(.05, global_step, 100, 0.95)
         optimizer = tf.train.AdagradOptimizer(learning_rate)
         gradients, variables = zip(*optimizer.compute_gradients(loss))
-        gradients, _ = tf.clip_by_global_norm(gradients, 5.0)
-        train_op = optimizer.apply_gradients(zip(gradients, variables))
+        gradients, _ = tf.clip_by_global_norm(gradients, 1.0)
+        train_op = optimizer.apply_gradients(zip(gradients, variables), global_step=global_step)
 
     # Predictions for the training, validation, and test data.
     with tf.variable_scope('prediction'):
@@ -78,7 +78,9 @@ def model_fn(tf_input_data, tf_ground_truth, n_channels):
 
     tf.summary.scalar('accuracy', accuracy(tf_ground_truth, tf_prediction))
 
-    return train_op, global_step, dice, loss
+    logit_norm = tf.norm(logits, ord=2)
+
+    return train_op, global_step, dice, loss, logit_norm
 
 
 def parse_example(serialized_example):

trainer/task.py
@@ -18,7 +18,7 @@ def run(target, is_chief, train_steps, job_dir, file_path, num_epochs):
             features, labels = model.input_fn(file_path, num_epochs)
 
             # Returns the training graph and global step tensor
-            train_op, global_step, dice, loss = model.model_fn(features, labels, num_channels)
+            train_op, global_step, dice, loss, g = model.model_fn(features, labels, num_channels)
 
         # Creates a MonitoredSession for training
         # MonitoredSession is a Session-like object that handles
@@ -39,11 +39,12 @@ def run(target, is_chief, train_steps, job_dir, file_path, num_epochs):
             # the global step tensor.
             # When train epochs is reached, session.should_stop() will be true. does nothing without queues
             while (train_steps is None or step < train_steps) and not session.should_stop():
-                step, _, dice_coef, l = session.run([global_step, train_op, dice, loss])
+                step, _, dice_coef, l, g_ = session.run([global_step, train_op, dice, loss, g])
                 if step % 1 == 0:
                     tf.logging.info('Step: {0}'.format(step))
                     tf.logging.info('Loss: {0}'.format(l))
                     tf.logging.info('Dice Coefficient: {0}'.format(dice_coef))
+                    tf.logging.info('Norm of logits: {0}'.format(g_))
 
 
 def dispatch(*args, **kwargs):